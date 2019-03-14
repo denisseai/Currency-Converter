3f61f3f57590d948b7ea0c888b307a29af3cb289
denisseai <msdenisseanaya@gmail.com>
2019-03-14 15:32:41 -0700

    Currency converter pounds to dollars

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+
+        Log.i("Info", "Button pressed");
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+
+        String amountInPounds = editText.getText().toString();
+
+        double amountInPoundsDouble = Double.parseDouble(amountInPounds);
+
+        double amountInDollarsDouble  = amountInPoundsDouble * 1.3;
+
+        String amountInDollarsString = String.format("%.2f", amountInDollarsDouble);
+
+        Log.i("Amount in dollars", amountInDollarsString);
+
+        Toast.makeText(this, "£" + amountInPounds + " is $" + amountInDollarsString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
